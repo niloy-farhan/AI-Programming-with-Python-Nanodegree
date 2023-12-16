6e00c71c1fa8093abbfdaefe5cad9662c93d0b2a
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-16 01:24:59 -0500

    update

--- image_classification/check_images.py
+++ image_classification/check_images.py
@@ -34,20 +34,39 @@ def main():
     start_time = time()
     end_time = time()
     tot_time = end_time - start_time
+    print("\n** Total Elapsed Runtime:",
+          str(int((tot_time / 3600))) + ":" + str(int((tot_time % 3600) / 60)) + str(int((tot_time % 3600) % 60)))
+
+def get_input_args():
+    """
+    Retrieves and parses the command line arguments created and defined using
+    the argparse module. This function returns these arguments as an
+    ArgumentParser object.
+     3 command line arguments are created:
+       dir - Path to the pet image files(default- 'pet_images/')
+       arch - CNN model architecture to use for image classification(default-
+              pick any of the following vgg, alexnet, resnet)
+       dogfile - Text file that contains all labels associated to dogs(default-
+                'dognames.txt'
+    Parameters:
+     None - simply using argparse module to create & store command line arguments
+    Returns:
+     parse_args() -data structure that stores the command line arguments object
+    """
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--dir', type=str, default='my_folder/',
                         help='path to the folder my_folder')
 
+    parser.add_argument("--arch", type=str, default='vgg',
+                        help='chosen model')
+
     parser.add_argument('--num', type=int, default=1,
                         help='Number (integer) input')
     in_args = parser.parse_args()
 
     print("Argument 1:", in_args.dir, " Argument 2:", in_args.num)
-
-    print("\n** Total Elapsed Runtime:",
-          str(int((tot_time / 3600))) + ":" + str(int((tot_time % 3600) / 60)) + str(int((tot_time % 3600) % 60)))
 
 
 if __name__ == "__main__":
